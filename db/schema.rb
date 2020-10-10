0c61be301a4ce3af701e33ab784ff06d5428c39c
kikuchi-gif <tea_left_result_29@yahoo.co.jp>
2020-10-10 16:41:06 +0000

    DRY

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_055036) do
+ActiveRecord::Schema.define(version: 2020_10_10_162159) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
