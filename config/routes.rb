d80bcb1318b8bf4d7ebbed86efd4f09be06594d7
kikuchi-gif <tea_left_result_29@yahoo.co.jp>
2020-10-17 10:26:30 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'toppages#index'
   get 'toppages/index'
-  root to: 'tasks#index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
 
   resources :tasks
 end
